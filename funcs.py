b999da4453c3eca9612b3a5d7ea1026759e35268
tim154753 <94345691+tim154753@users.noreply.github.com>
2024-07-12 03:18:02 -0700

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -1,6 +1,9 @@
 import numpy as np
 import backpropfuncs as bp
-
+import matplotlib.pyplot as plt
+import time
+import tkinter as tk
+from PIL import Image, ImageDraw
 #layers size cannot change after network initialization
 
 
@@ -10,7 +13,7 @@ class Network:
         self.weights = [np.zeros((args[i+1], args[i])) for i in range(len(args)-1)]
         self.biases = [np.zeros(arg) for arg in args[1:]]
         self.weighted_inputs = [np.zeros(arg) for arg in args]
-        self.errors = [np.zeros(arg) for arg in args[1:]]
+        self.errors = [np.zeros(args[i+1]) for i in range(len(args)-1)]
         self.weight_gradients = [np.zeros((args[i+1], args[i])) for i in range(len(args)-1)]
         self.bias_gradients = [np.zeros(arg) for arg in args[1:]]
         self.num_layers = len(args)
@@ -20,8 +23,8 @@ class Network:
         result = "This is the network"
         for i, layer in enumerate(self.layers):
             result += f"\n Layer {i} values: {layer}\n"
-        for weights, biases in zip(self.weights, self.biases):
-            result += f"\nWeights:\n--{weights}\nBiases:\n--{biases}"
+        for weights, biases, errors in zip(self.weights, self.biases, self.errors):
+            result += f"\nWeights:\n--{weights}\nBiases:\n--{biases}\nErrors:\n{errors}"
         return result
 
     def initialize_wb(self):
@@ -40,30 +43,126 @@ class Network:
         self.weighted_inputs[-1] = output_layer
         self.layers[-1] = bp.softmax(output_layer)
 
-    def normalize(self):
-        self.layers[0] /= 255
-
-    def backprop(self, label, layer_index = None):
+    def backprop(self, label):
         self.errors[-1] = bp.output_layer_error(self.layers[-1], label)
-        self.weight_gradients[-1] = bp.find_weight_grad(self, -1)
-        self.bias_gradients[-1].fill(0)
-        self.bias_gradients[-1] = np.add(self.bias_gradients[-1], self.errors)
+        #print(f"From backprop: this is error[-1]:\n{self.errors[-1]}")
+        self.weight_gradients[-1] = np.add(self.weight_gradients[-1], bp.find_weight_grad(self, -1))
+        #self.bias_gradients[-1].fill(0)
+        self.bias_gradients[-1] = np.add(self.bias_gradients[-1], self.errors[-1])
         for i in range(len(self.errors) - 2, 0, -1):
+            #print(f"From backprop: this is weights for layer {i} (shape = {np.shape(self.weights[i])}:\n{self.weights[i]}")
             self.errors[i] = bp.error_from_next_layer(self, i)
-            self.weight_gradients[i] = bp.find_weight_grad(self, i)
-            self.bias_gradients[i].fill(0)
+            #print(f"From backprop: this is error {i}:\n{self.errors[i]}")
+            self.weight_gradients[i] = np.add(self.weight_gradients[i], bp.find_weight_grad(self, i))
+            #self.bias_gradients[i].fill(0)
             self.bias_gradients[i] = np.add(self.bias_gradients[i], self.errors[i])
+
 
     def update(self):
         for i in range(len(self.weights)):
             self.weights[i] = np.subtract(self.weights[i], self.weight_gradients[i] * self.learning_rate)
             self.biases[i] = np.subtract(self.biases[i], self.bias_gradients[i] * self.learning_rate)
-    
 
-network = Network(3,5,2, learning_rate=0.01)
-network.layers[0] = np.random.randn(*np.shape(network.layers[0]))
+
+    def mini_batch_gradient_descent(self, batch_size, images, labels, num_epochs):
+        for epoch in range(num_epochs):
+            print(f"\nEPOCH: {epoch}\n")
+            indices = np.arange(images.shape[0])
+            np.random.shuffle(indices)
+            images = images[indices]
+            labels = labels[indices]
+            for k in range(len(self.weight_gradients)):
+                self.weight_gradients[k].fill(0)
+                self.bias_gradients[k].fill(0)
+            for i in range(0, len(images), batch_size):
+                #print(f"On batch {i}!\n")
+                batch_images = images[i:i+batch_size]
+                batch_labels = labels[i:i+batch_size]
+                for j in range(len(batch_images)):
+                    self.layers[0] = batch_images[j]
+                    self.generate()
+                    self.backprop(batch_labels[j])
+                for p in range(len(self.weight_gradients)):
+                    self.weight_gradients[p] /= batch_size
+                    self.bias_gradients[p] /= batch_size
+                self.update()
+
+
+network = Network(784, 100, 10, learning_rate=0.01)
 network.initialize_wb()
 print(network)
-network.generate()
-print(network)
-network.backprop(2)
+
+images = bp.load_all_images()
+labels = bp.load_all_labels()
+test_images = bp.load_all_test_images()
+test_labels = bp.load_all_test_labels()
+label_nums = bp.load_all_test_labels("nums")
+start = time.time()
+network.mini_batch_gradient_descent(50, images, labels, 10)
+end = time.time()
+print(f"Took {end-start} sec to train")
+
+
+count = 0
+for i in range(10000):
+    test_label = test_labels[i]
+    network.layers[0] = test_images[i]
+    network.generate()
+    if bp.is_correct(label_nums[i], network.layers[-1]):
+        count += 1
+
+print(f"Classified {count} correctly out of 10000 = {count/100}% accurate")
+
+'''
+number = int(input("Image number? (type \"-1\" to stop)\n"))
+while(number != -1):
+    network.layers[0] = images[number]
+    network.generate()
+    pixels = images[number]
+    image = pixels.reshape((28,28))
+    plt.imshow(image, cmap='gray')
+    plt.show()
+    print(np.argmax(network.layers[-1]))
+    number = int(input("Image number? (type \"-1\" to stop)\n"))
+'''
+print(images[10])
+
+window = tk.Tk()
+canvas = tk.Canvas(window, width = 500, height = 500)
+canvas.pack()
+image = Image.new("L", (500,500), "black")
+draw = ImageDraw.Draw(image)
+def draw_on_canvas(event):
+    radius = 10
+    x1, y1 = (event.x - radius), (event.y - radius)
+    x2, y2 = (event.x + radius), (event.y + radius)
+    canvas.create_oval(x1, y1, x2, y2, fill='white')
+    #draw.line([x1, y1, x2, y2], fill="black", width=50)
+    draw.ellipse([x1,y1,x2,y2], fill="white")
+canvas.bind("<B1-Motion>", draw_on_canvas)
+
+def classify_image():
+    resized_image = image.resize((28,28)).convert("L")
+    image_pixels = np.asarray(resized_image) / 255.0
+    image_pixels[image_pixels < 0.1] = 0
+    plt.imshow(image_pixels*255, cmap = 'gray')
+    plt.show()
+    image_pixels = image_pixels.reshape(784,)
+    #print(image_pixels)
+    network.layers[0] = image_pixels
+    network.generate()
+    print(np.argmax(network.layers[-1]))
+
+def clear_image():
+    canvas.delete("all")
+    global image, draw
+    image = Image.new("RGB", (500,500), "black")
+    draw = ImageDraw.Draw(image)
+
+classify_button = tk.Button(window, text = "Classify", command=classify_image)
+classify_button.pack()
+
+clear_button = tk.Button(window, text = "Clear", command=clear_image)
+clear_button.pack()
+
+window.mainloop()
